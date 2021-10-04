b35fb16b4f7334225583c42fad49f1f34a7a881b
lubianat <tiago.lubiana.alves@usp.br>
2021-10-04 13:59:49 -0300

    Set up marker update for humans

--- curation_of_classes/reconcile_markers.py
+++ curation_of_classes/reconcile_markers.py
@@ -7,10 +7,11 @@ import time
 from wikidataintegrator import wdi_login, wdi_core
 import getpass
 import json
+import sys
 
-os.system(
-    "wget -O cell_classes.xlsx https://docs.google.com/spreadsheets/d/e/2PACX-1vTanLtzxD6OXpu3Ze4aNITlIMZEqfK3qrrcNiwFE6kA-YVnuzULp3dG3oYIe5gYAVj28QWZnGwzN_H6/pub\?output\=xlsx"
-)
+# os.system(
+#    "wget -O cell_classes.xlsx https://docs.google.com/spreadsheets/d/e/2PACX-1vTanLtzxD6OXpu3Ze4aNITlIMZEqfK3qrrcNiwFE6kA-YVnuzULp3dG3oYIe5gYAVj28QWZnGwzN_H6/pub\?output\=xlsx"
+# )
 
 cell_markers = pd.read_excel("cell_classes.xlsx", sheet_name="cell markers")
 
@@ -32,6 +33,7 @@ def split_markers(marker_string):
 with open("celltypes_dict.pickle", "rb") as handle:
     subclass_dict = pickle.load(handle)
 
+subclass_dict["human type 1 thymic fibroblast"] = "Q107446080"
 pwd = getpass.getpass()
 login_instance = wdi_login.WDLogin(user="TiagoLubiana", pwd=pwd)
 
@@ -58,10 +60,10 @@ for index, row in cell_markers.iterrows():
         value=row["stated in"], prop_nr="P248", is_reference=True
     )
 
-    stated_in_full_reference = wdi_core.WDItemID(
-        value=row["stated in"], prop_nr="P248", is_reference=True
+    stated_as_full_reference = wdi_core.WDString(
+        value=row["stated as"], prop_nr="P1932", is_reference=True
     )
-    references = [[stated_in_full_reference]]
+    references = [[stated_in_full_reference, stated_as_full_reference]]
 
     label = row["cell class"]
     print(f"Running code for {label} ")
@@ -70,7 +72,21 @@ for index, row in cell_markers.iterrows():
         if "human" in label:
             ## Look for human gene ID
             qid = travel_gene_dict(marker, "human")
-            print(qid)
+
+            try:
+                data_for_item.append(
+                    wdi_core.WDItemID(value=qid, prop_nr="P8872", references=references)
+                )
+                print(qid)
+                cell_type_id = subclass_dict[row["cell class"]]
+                print(cell_type_id)
+                wd_item = wdi_core.WDItemEngine(
+                    wd_item_id=cell_type_id, data=data_for_item
+                )
+                print(wd_item)
+                wd_item.write(login_instance)
+            except:
+                print(f"Failed for {marker}")
 
         elif "mouse" in label:
             ## Look for mouse gene ID
@@ -81,3 +97,4 @@ for index, row in cell_markers.iterrows():
             pass
         else:
             description = "cell type"
+
