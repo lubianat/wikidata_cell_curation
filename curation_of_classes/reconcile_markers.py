db884da813195bda1aea4f6a7542b6e0cefbca71
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2021-10-15 11:36:00 -0300

    Set up integration for mouse genes

--- curation_of_classes/reconcile_markers.py
+++ curation_of_classes/reconcile_markers.py
@@ -33,7 +33,7 @@ def split_markers(marker_string):
 with open("celltypes_dict.pickle", "rb") as handle:
     subclass_dict = pickle.load(handle)
 
-subclass_dict["human type 1 thymic fibroblast"] = "Q107446080"
+subclass_dict["mouse glycynergic cell"] = "Q104416297"
 pwd = getpass.getpass()
 login_instance = wdi_login.WDLogin(user="TiagoLubiana", pwd=pwd)
 
@@ -75,13 +75,17 @@ for index, row in cell_markers.iterrows():
 
             try:
                 data_for_item.append(
-                    wdi_core.WDItemID(value=qid, prop_nr="P8872", references=references)
+                    wdi_core.WDItemID(
+                        value=qid,
+                        prop_nr="P8872",
+                        references=references,
+                    )
                 )
                 print(qid)
                 cell_type_id = subclass_dict[row["cell class"]]
                 print(cell_type_id)
                 wd_item = wdi_core.WDItemEngine(
-                    wd_item_id=cell_type_id, data=data_for_item
+                    wd_item_id=cell_type_id, data=data_for_item, append_value=["P8872"]
                 )
                 print(wd_item)
                 wd_item.write(login_instance)
@@ -89,8 +93,21 @@ for index, row in cell_markers.iterrows():
                 print(f"Failed for {marker}")
 
         elif "mouse" in label:
-            ## Look for mouse gene ID
-            pass
+            qid = travel_gene_dict(marker, "mouse")
+
+            data_for_item.append(
+                wdi_core.WDItemID(value=qid, prop_nr="P8872", references=references)
+            )
+            print(qid)
+            cell_type_id = subclass_dict[row["cell class"]]
+            print(cell_type_id)
+            wd_item = wdi_core.WDItemEngine(
+                wd_item_id=cell_type_id,
+                data=data_for_item,
+                append_value=["P8872"],
+            )
+            print(wd_item)
+            wd_item.write(login_instance)
 
         elif "zebrafish" in label:
             ## Look for zebrafish gene id
@@ -98,3 +115,4 @@ for index, row in cell_markers.iterrows():
         else:
             description = "cell type"
 
+        break
