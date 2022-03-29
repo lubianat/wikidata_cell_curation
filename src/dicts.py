19ab80ed0ba27b63bc522477a4b1f3f27bbf5e8b
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-03-29 09:20:52 -0300

    Update dictionaries

--- src/dicts.py
+++ src/dicts.py
@@ -79,37 +79,3 @@ instance_dict = {
     "cell class":"Q104852483",
     "cell state":"Q104539563"
 }
-
-part_of_dict = {
-    "ventral cochlear nucleus (VCN)":"Q4771335",
-    "placental villi":"Q2272222",
-    "fetal liver":"Q104539102",
-    "epididymis": "Q1973610",
-    "prostate": "Q9625",
-    "spleen": "Q9371",
-    "epidermis": "Q796482",
-    "thymus": "Q163987",
-    "thymic medulla": "Q107446186",
-    "interfollicular epidermis": "Q106385904",
-    "hair follicle": "Q866324",
-    "peritoneal cavity": "Q1030169",
-    "liver":"Q9368",
-    "airways": "Q13400765",
-    "bronchius": "Q181602",
-    "esophagus": "Q173710",
-    "retina": "Q169342",
-    "brain": "Q1073",
-    "lung": "Q7886",
-    "sciatic nerve": "Q307869",
-    "cochlea" : "Q317857",
-    "spiral ganglion": "Q3095145",
-    "dorsal root ganglion": "Q1395415",
-    "colon": "Q5982337",
-    "kidney": "Q9377",
-    "dura mater": "Q1394389",
-    "pancreas": "Q9618",
-    "placenta": "Q1212935",
-    "intestine": "Q9639",
-    "skin": "Q1074"
-
-}
