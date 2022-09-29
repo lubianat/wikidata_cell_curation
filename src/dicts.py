573fdca0d4a0fa38ab9e04632b1e7904457a4e7f
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-09-29 09:11:53 -0300

    Update available species

--- src/dicts.py
+++ src/dicts.py
@@ -1,7 +1,8 @@
 import json
 import os, json
+from collections import OrderedDict
 
-dicts = {}
+DICTS = {}
 
 path_to_json = "dictionaries"
 json_files = [
@@ -11,89 +12,37 @@ json_files = [
 for json_file in json_files:
     keyword = json_file.split(".")[0]
     with open(f"dictionaries/{keyword}.json") as f:
-        dicts[keyword] = json.loads(f.read())
+        DICTS[keyword] = json.loads(f.read())
 
-
-# List is needed so parsing is ordered. 
-# Names might be nested (e.g. "naked mole rat" and "rat")
-species_list = ["mouse", "human", "zebrafish", "sheep", "marmoset", "hamster", "macaque", "rat", "Middle East blind mole rat"]
-species_list.sort(key = len)
-species_list.reverse()
-
-species_dict = {
-
-    "mouse" :{
-        "qid": "Q83310",
-        "species_name": "Mus musculus"
-    }, 
-
-    "human": {
-        "qid":"Q15978631",
-        "species_name": "Homo sapiens"
-    }, 
-
-    "zebrafish": {
-        "qid":"Q15978631",
-        "species_name": "Danio rerio"
-    },
-
-    "sheep": {
-        "qid": "Q29350771",
-        "species_name": "Ovis aries"
-    },
-
-    "marmoset": {
-        "qid":"Q838947",
-        "species_name": "Callithrix jacchus"
-    },
-
-    "macaque": {
-        "qid": "Q301676",
-        "species_name": "Macaca fascicularis"
-    },
-
-
-    "rhesus": {
-        "qid": "Q156606",
-        "species_name": "Macaca mulatta"
-    },
-    "hamster": {
-        "qid": "Q204175",
-        "species_name": "Mesocricetus auratus"
-    },
-
-    "rat": {
-        "qid": "Q184224",
-        "species_name": "Rattus norvegicus"
-    },
-
+species_dict = OrderedDict{
+    "mouse": {"qid": "Q83310", "species_name": "Mus musculus"},
+    "human": {"qid": "Q15978631", "species_name": "Homo sapiens"},
+    "zebrafish": {"qid": "Q15978631", "species_name": "Danio rerio"},
+    "sheep": {"qid": "Q29350771", "species_name": "Ovis aries"},
+    "marmoset": {"qid": "Q838947", "species_name": "Callithrix jacchus"},
+    "macaque": {"qid": "Q301676", "species_name": "Macaca fascicularis"},
+    "rhesus": {"qid": "Q156606", "species_name": "Macaca mulatta"},
+    "hamster": {"qid": "Q204175", "species_name": "Mesocricetus auratus"},
+    "rat": {"qid": "Q184224", "species_name": "Rattus norvegicus"},
     "Middle East blind mole rat": {
         "qid": "Q13422405",
-        "species_name": "Spalax ehrenbergi"
+        "species_name": "Spalax ehrenbergi",
     },
-
-    "Nematostella vectensis" : {
+    "Nematostella vectensis": {
         "qid": "Q139440",
-        "species_name": "Nematostella vectensis"
-
-    }, 
-    "primate " : {
-        "qid": "Q7380",
-        "species_name": "primates"
-
+        "species_name": "Nematostella vectensis",
     },
-    "murine " : {
-        "qid": "Q30266",
-        "species_name": "murines"
-
-    },  
-
+    "primate ": {"qid": "Q7380", "species_name": "primates"},
+    "reptilian": {"qid":"Q10811", "species_name": "reptiles"},
+    "murine ": {"qid": "Q30266", "species_name": "murines"},
+    "Trachemys scripta elegans": {"qid": "Q207839", "species_name": "Trachemys scripta elegans"},
+    "Pogona vitticeps": {"qid":"Q792301", "species_name":"Pogona vitticeps"}
 }
 
 instance_dict = {
-    "cell type":"Q189118",
-    "cell class":"Q104852483",
-    "cell state":"Q104539563",
+    "cell type": "Q189118",
+    "cell class": "Q104852483",
+    "cell state": "Q104539563",
     "lipotype": "Q111649907",
-    "Q111649907": "Q111649907"
+    "Q111649907": "Q111649907",
 }
